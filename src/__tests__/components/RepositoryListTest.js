4d4b6d824a751080626d059ddc347f3d7b42c199
Johannes Welsch <welschmoor@gmail.com>
2022-04-19 00:25:36 +0200

    selectById does not work, investigating...

---
+++ src/__tests__/components/RepositoryListTest.js
@@ -0,0 +1,29 @@
+
+// npm run test --src/__tests__/components/RepositoryListTest.jsx
+// import React from 'react';
+// import { render } from '@testing-library/react-native';
+
+// import RepositoryListContainer from '../../components/RepositoryList/RepositoryListContainer';
+
+
+// import _ from 'lodash'
+// have to create this 
+// function getAllByTestId(id) {
+//   const instances = getAllByProps({ testID: id });
+//   console.log("_.uniqBy(instances, instance => instance.props.children)", _.uniqBy(instances, instance => instance.props.children))
+//   return _.uniqBy(instances, instance => instance.props.children);
+// }
+
+describe("blah", function () {
+  it("testing test", function () {
+    expect(1).toBe(1)
+  })
+
+  // it.only("element exists?", function () {
+  //   // const repositoryItems = getAllByTestId('repositoryItem');
+  //   // const [firstRepositoryItem, secondRepositoryItem] = repositoryItems;
+  //   // console.log("repositoryItems<><>", repositoryItems)
+
+  //   expect(1).toBe(1)
+  // })
+})
